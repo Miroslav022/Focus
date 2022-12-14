c00c579735cf7abd83000e10b57ddf6d10650c92
Miroslav022 <95857090+Miroslav022@users.noreply.github.com>
2022-12-14 21:22:44 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 let wLoaction = window.location.pathname;
-if (wLoaction.includes("index.html") || wLocation=='/') {
+if (wLoaction.includes("index.html") || wLocation.'/Focus/') {
   let navLink = ["Home", "About us", "Projects", "Author"];
   let links = ["index.html", "#wwa", "#project-section", "author.html"];
   let navBar = document.querySelector(".navBar");
